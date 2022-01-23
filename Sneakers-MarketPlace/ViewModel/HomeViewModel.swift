b7723e9ba4aea0368392422995b755dddf862056
amirnilsson <amirnilsson@gmail.com>
2022-01-23 23:08:47 +0100

    added some styling

---
+++ Sneakers-MarketPlace/ViewModel/HomeViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewModel.swift
+//  Sneakers-MarketPlace
+//
+//  Created by Amir Nilsson on 2022-01-23.
+//
+
+import SwiftUI
+
+struct HomeViewModel: View {
+    var body: some View {
+        
+    Text("hellow world")
+        
+    }
+}
+
+struct HomeViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewModel()
+    }
+}
