5b3d5ec37890c027245ce1abbb37089930e0c185
amirnilsson <amirnilsson@gmail.com>
2022-02-05 21:17:13 +0100

    added itemView and items in fireStore

--- Sneakers-MarketPlace/ViewModel/HomeViewModel.swift
+++ Sneakers-MarketPlace/ViewModel/HomeViewModel.swift
@@ -7,6 +7,9 @@
 
 import SwiftUI
 import CoreLocation
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 class HomeViewModel : NSObject ,ObservableObject, CLLocationManagerDelegate {
     
@@ -18,8 +21,12 @@ class HomeViewModel : NSObject ,ObservableObject, CLLocationManagerDelegate {
     @Published var userAdress = ""
     @Published var noLocation = false
     
+    
     // side bar
     @Published var showSideBar = false
+    
+    // itemData
+    @Published var items: [Item] = []
     
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         //check location acess
@@ -74,5 +81,66 @@ class HomeViewModel : NSObject ,ObservableObject, CLLocationManagerDelegate {
         }
         
     }
+    
+    // fetching Data
+    
+    func fetchData() {
+        
+        let db = Firestore.firestore()
+        
+        db.collection("Items").getDocuments { (snap, err) in
+            guard let itemData = snap else {return}
+            
+            print("before line 93")
+            self.items = itemData.documents.compactMap({  (doc) -> Item? in
+                print("after line 93")
+                
+                let id = doc.documentID
+                let name = doc.get("item_name") as! String
+                let cost = doc.get("item_cost") as! NSNumber
+                let ratings = doc.get("item_rating") as! String
+                let image = doc.get("item_image") as! String
+                let details = doc.get("item_details") as! String
+                
+                print("just before line 103")
+                return Item(id: id, item_name: name, item_cost: cost, item_details: details, item_image: image, item_rating: ratings)
+            })
+        }
+    }
+    
+    func fetchData2() {
+        
+        let db = Firestore.firestore()
+        
+        db.collection("Items").addSnapshotListener{ (querySnapshot, error ) in
+            guard let documents = querySnapshot?.documents else {
+                print("no documents")
+                return
+            }
+            self.items = documents.map{(queryDocumentSnapshot) -> Item in
+                let data = queryDocumentSnapshot.data()
+                
+                let id = queryDocumentSnapshot.documentID
+                let name = data["item_name"] as! String
+                let cost = data["item_cost"] as! NSNumber
+                let ratings = data["item_rating"] as! String
+                let image = data["item_image"] as! String
+                let details = data["item_details"] as! String
+                
+                print("right before returning item")
+                
+                return Item(id: id, item_name: name, item_cost: cost, item_details: details, item_image: image, item_rating: ratings)
+            }
+        }
+    }
+    
+    
+     func checkLoginForData() {
+        
+            self.fetchData2()
+            print("fetchData func ran")
+        
+    }
+    
     
 }
